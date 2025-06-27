5f597589a9a702506782c3ef982f33054df2e40a
Anderson Dávila <andersonfidel.14@gmail.com>
2025-06-26 23:08:19 -0400

    Add VueFlow components and update routing
    - Introduced EdgeComponent and NodeComponent for custom edge and node rendering.
    - Created VueFlowPage to demonstrate usage of custom components.
    - Updated routes to include a demo path for VueFlowPage.
    - Enhanced .prettierrc configuration for consistent code formatting.
    - Updated package.json with new dependencies for VueFlow and related packages.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,15 +4,20 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }]
+  },
+  {
+    path: '/demo',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/VueFlowPage.vue') }]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    component: () => import('pages/ErrorNotFound.vue')
+  }
 ];
 
 export default routes;
